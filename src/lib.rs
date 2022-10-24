efd80b66dcbc904d92276ebf770c65819fa54f72
James Bradlee <post@jamesb.no>
2022-10-24 23:39:15 +0200

    Rename GothamState to State

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,12 @@ use std::any::TypeId;
 use std::collections::BTreeMap;
 
 #[derive(Default)]
-pub struct GothamState {
+pub struct State {
 	data: BTreeMap<TypeId, Box<dyn Any>>,
 }
 
-impl GothamState {
-	/// Puts a value into the `GothamState` storage. One value of each type is retained.
+impl State {
+	/// Puts a value into the `State` storage. One value of each type is retained.
 	/// Successive calls to `put` will overwrite the existing value of the same
 	/// type.
 	pub fn put<T: 'static>(&mut self, t: T) {
@@ -27,37 +27,37 @@ impl GothamState {
 		self.data.insert(type_id, Box::new(t));
 	}
 
-	/// Determines if the current value exists in `GothamState` storage.
+	/// Determines if the current value exists in `State` storage.
 	pub fn has<T: 'static>(&self) -> bool {
 		let type_id = TypeId::of::<T>();
 		self.data.get(&type_id).is_some()
 	}
 
-	/// Tries to borrow a value from the `GothamState` storage.
+	/// Tries to borrow a value from the `State` storage.
 	pub fn try_borrow<T: 'static>(&self) -> Option<&T> {
 		let type_id = TypeId::of::<T>();
 		trace!(" borrowing state data for type_id `{:?}`", type_id);
 		self.data.get(&type_id).and_then(|b| b.downcast_ref())
 	}
 
-	/// Borrows a value from the `GothamState` storage.
+	/// Borrows a value from the `State` storage.
 	pub fn borrow<T: 'static>(&self) -> &T {
 		self.try_borrow().unwrap_or_else(|| missing::<T>())
 	}
 
-	/// Tries to mutably borrow a value from the `GothamState` storage.
+	/// Tries to mutably borrow a value from the `State` storage.
 	pub fn try_borrow_mut<T: 'static>(&mut self) -> Option<&mut T> {
 		let type_id = TypeId::of::<T>();
 		trace!(" mutably borrowing state data for type_id `{:?}`", type_id);
 		self.data.get_mut(&type_id).and_then(|b| b.downcast_mut())
 	}
 
-	/// Mutably borrows a value from the `GothamState` storage.
+	/// Mutably borrows a value from the `State` storage.
 	pub fn borrow_mut<T: 'static>(&mut self) -> &mut T {
 		self.try_borrow_mut().unwrap_or_else(|| missing::<T>())
 	}
 
-	/// Tries to move a value out of the `GothamState` storage and return ownership.
+	/// Tries to move a value out of the `State` storage and return ownership.
 	pub fn try_take<T: 'static>(&mut self) -> Option<T> {
 		let type_id = TypeId::of::<T>();
 		trace!(
@@ -70,11 +70,11 @@ impl GothamState {
 			.map(|b| *b)
 	}
 
-	/// Moves a value out of the `GothamState` storage and returns ownership.
+	/// Moves a value out of the `State` storage and returns ownership.
 	///
 	/// # Panics
 	///
-	/// If a value of type `T` is not present in `GothamState`.
+	/// If a value of type `T` is not present in `State`.
 	pub fn take<T: 'static>(&mut self) -> T {
 		self.try_take().unwrap_or_else(|| missing::<T>())
 	}
@@ -82,7 +82,7 @@ impl GothamState {
 
 fn missing<T: 'static>() -> ! {
 	panic!(
-		"required type {} is not present in GothamState container",
+		"required type {} is not present in State container",
 		type_name::<T>()
 	);
 }
